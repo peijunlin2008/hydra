bad55dc1856794b17d230e5c03060f53289da8b3
zkfy <lib4net@163.com>
2021-05-09 08:48:35 +0800

    添加docs

--- global/rid_test.go
+++
@@ -1,9 +0,0 @@
-package global
-
-import "testing"
-
-func BenchmarkCreate(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		RID.GetXRequestID()
-	}
-}
--- hydra/cmds/pub/script_windows.go
+++
@@ -1,5 +0,0 @@
-package pub
-
-func getScript() (string, string) {
-	return "", ""
-}
